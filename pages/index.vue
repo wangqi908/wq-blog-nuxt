84e4d5178eb9cea604103c01b61f349c4bfba0be
wangqi <wangqi908@hotmail.com>
2020-06-08 13:10:18 +0800

    初始化

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <List />
+    <button @click="$router.push({ path: '/post/view' })">跳转</button>
+    <div class="test">aaa</div>
+  </div>
+</template>
+
+<script>
+import List from '@/components/List.vue'
+
+export default {
+  components: {
+    List
+  }
+}
+</script>
+
+<style lang='scss'>
+.test {
+  background-color: $headBgc;
+}
+</style>
---
+++ pages/post/view/_id.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>view</p>
+    <div v-html="content"></div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      content: 'aaaa'
+    }
+  },
+  asyncData() {
+    return axios
+      .post(`/blog/post/view`, {
+        _id: '5ed5ddc30fbb844b24b4bcdb'
+      })
+      .then(res => {
+        // this.content = res.data.data.content
+        return { content: res.data.data.content }
+      })
+  }
+}
+</script>
+
+<style>
+</style>
